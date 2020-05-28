5de46dc8d738bf230aef2c0836d2e0c404c4778b
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2020-05-28 16:34:19 -0500

    Implementacion de la busqquda!

--- controller/lookup.php
+++ controller/lookup.php
@@ -1,12 +1,139 @@
 <?php
 include_once '../models/dbh.php';
+session_start();
 
-$lugar = $_POST[''];
-$rango = $_POST[''];
-$precioMin = $_POST[''];
-$precioMax = $_POST[''];
-$recamaras = $_POST[''];
-$banos = $_POST[''];
+$lugar = $_POST['CEoCP'];
+$rango = $_POST['rango'];
+$precioMin = $_POST['precio_min'];
+$precioMax = $_POST['precio_max'];
+$recamaras = $_POST['rooms'];
+$banos = $_POST['baths'];
 
+$all = "'$lugar', '$rango', '$precioMin', '$precioMax', '$recamaras', '$banos'";
+
+$sql = "select usuarios.*, ubicacion.*, casas.*\n"
+. "from casas\n"
+. "INNER JOIN ubicacion ON casas.fk_ubicacionID = ubicacion.ubicacionID\n"
+. "INNER JOIN usuarios ON casas.fk_usuarioID = usuarios.usuariosID\n"
+. "WHERE";
+
+// Lugar
+if ($lugar != '') {
+  $sql .= " ubicacion.country LIKE \"%$lugar%\" or ubicacion.state LIKE \"%$lugar%\" or ubicacion.city LIKE \"%$lugar%\"\n"
+  . "AND ";
+}
+
+
+// Rango
+if ( $rango != 0 ) {
+  if ( $rango == 1 ) {
+    $sql .= " ubicacion.distancia <= 5";
+  }
+  else if ( $rango == 2 ) {
+    $sql .= " ubicacion.distancia <= 10";
+  }
+  else if ( $rango == 3 ) {
+    $sql .= " ubicacion.distancia <= 20";
+  }
+
+  $sql .= "\n AND ";
+}
+
+// Precio min
+if ( $precioMin != 0 ) {
+  if ( $precioMin == 1 ) {
+    $sql .= " casas.price >= 100000";
+  }
+  else if ( $precioMin == 2 ) {
+    $sql .= " casas.price >= 500000";
+  }
+  else if ( $precioMin == 3 ) {
+    $sql .= " casas.price >= 1000000";
+  }
+  else if ( $precioMin == 4 ) {
+    $sql .= " casas.price >= 2000000";
+  }
+
+  $sql .= "\n AND ";
+}
+
+// Precio max
+if ( $precioMax != 0 ) {
+  if ( $precioMax == 1 ) {
+    $sql .= " casas.price <= 100000";
+  }
+  else if ( $precioMax == 2 ) {
+    $sql .= " casas.price <= 500000";
+  }
+  else if ( $precioMax == 3 ) {
+    $sql .= " casas.price <= 1000000";
+  }
+  else if ( $precioMax == 4 ) {
+    $sql .= " casas.price <= 2000000";
+  }
+
+  $sql .= "\n AND ";
+}
+
+// Recamaras
+if ( $recamaras != 0 ) {
+  if ( $recamaras == 1 ) {
+    $sql .= " casas.rooms >= 1";
+  }
+  else if ( $recamaras == 2 ) {
+    $sql .= " casas.rooms >= 2";
+  }
+  else if ( $recamaras == 3 ) {
+    $sql .= " casas.rooms >= 3";
+  }
+
+  $sql .= "\n AND ";
+}
+
+// Banos
+if ( $banos != 0 ) {
+  if ( $banos == 1 ) {
+    $sql .= " casas.bathrooms >= 1";
+  }
+  else if ( $banos == 2 ) {
+    $sql .= " casas.bathrooms >= 1";
+  }
+  else if ( $banos == 3 ) {
+    $sql .= " casas.bathrooms >= 2";
+  }
+  else if ( $banos == 4 ) {
+    $sql .= " casas.bathrooms >= 3";
+  }
+
+  $sql .= "\n AND ";
+}
+
+
+$sql .= " 1 = 1;";
+//echo $sql;
+
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+
+$resultCheck = mysqli_num_rows($result);
+
+$array = array();
+
+if ( $resultCheck > 0 ) {
+  while ( $row = mysqli_fetch_assoc( $result ) ) {
+    $str = "";
+    $str .= $row['name'] . " " . $row['distancia'] . " " . $row['country'] . " " .$row['state'] . " ";
+    $str .= $row['city'] . " " . $row['price'] . " " . $row['rooms'] . " " . $row['bathrooms'] . " " . "<br>";
+    array_push($array, $str);
+  }
+}
+
+// foreach ($array as &$valor) {
+//   echo $valor;
+// }
+
+$_SESSION['resultado'] = $array;
+header("Location: ../views/busqueda.php");
 
  ?>
--- includes/Busqueda.php
+++ includes/Busqueda.php
@@ -1,4 +1,4 @@
-<form action="../controller/buscar.php" method="post" onsubmit="">
+<form action="../controller/lookup.php" method="post" onsubmit="">
   <div class="row">
     <div class="lupa">
       <img src="../includes/img/img_lupa.png">
@@ -10,17 +10,19 @@
   <br>
   <label>Ciudad y Estado, o C.P.</label>
   <input type="text" name="CEoCP">
+
   <label>Rango De Busqueda</label><br>
-  <select>
+  <select name = "rango">
     <option value="0">Sin Rango</option>
     <option value="1">5km</option>
     <option value="2">10km</option>
     <option value="3">20km</option>
   </select>
+
   <label>Rango de Precio De : - A:</label>
   <div class="row">
     <div class="De">
-      <select>
+      <select name = "precio_min">
         <option value="0">Todos</option>
         <option value="1">100,000</option>
         <option value="2">500,000</option>
@@ -29,7 +31,7 @@
       </select>
     </div>
     <div class="A">
-      <select>
+      <select name = "precio_max">
         <option value="0">Todos</option>
         <option value="1">100,000</option>
         <option value="2">500,000</option>
@@ -38,15 +40,17 @@
       </select>
     </div>
   </div>
+
   <label>Recámara(s)</label><br>
-  <select>
+  <select name = "rooms">
     <option value="0">No importa.</option>
     <option value="1">3</option>
     <option value="2">4</option>
     <option value="3">5</option>
   </select>
+
   <br><label>Baño(s)</label><br>
-  <select>
+  <select name = "baths">
     <option value="0">No importa.</option>
     <option value="1">1</option>
     <option value="2">1 y medio</option>
