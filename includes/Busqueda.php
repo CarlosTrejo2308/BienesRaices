626b0995cf9599eccdde5bf1e9eb768e1e764f8e
David Dark <32287340+DavidDark@users.noreply.github.com>
2020-05-27 20:44:42 -0500

    Push-n+1

--- includes/Busqueda.php
+++ includes/Busqueda.php
@@ -1,59 +1,61 @@
-<div class="row">
-  <div class="lupa">
-    <img src="../includes/img/img_lupa.png">
+<form action="../controller/buscar.php" method="post" onsubmit="">
+  <div class="row">
+    <div class="lupa">
+      <img src="../includes/img/img_lupa.png">
+    </div>
+    <div class="encuentra_tu_casa">
+      <img src="../includes/img/encuentra tu  futura propiedad .png">
+    </div>
   </div>
-  <div class="encuentra_tu_casa">
-    <img src="../includes/img/encuentra tu  futura propiedad .png">
+  <br>
+  <label>Ciudad y Estado, o C.P.</label>
+  <input type="text" name="CEoCP">
+  <label>Rango De Busqueda</label><br>
+  <select>
+    <option value="0">Sin Rango</option>
+    <option value="1">5km</option>
+    <option value="2">10km</option>
+    <option value="3">20km</option>
+  </select>
+  <label>Rango de Precio De : - A:</label>
+  <div class="row">
+    <div class="De">
+      <select>
+        <option value="0">Todos</option>
+        <option value="1">100,000</option>
+        <option value="2">500,000</option>
+        <option value="3">1,000,000</option>
+        <option value="4">2,000,000</option>
+      </select>
+    </div>
+    <div class="A">
+      <select>
+        <option value="0">Todos</option>
+        <option value="1">100,000</option>
+        <option value="2">500,000</option>
+        <option value="3">1,000,000</option>
+        <option value="4">2,000,000</option>
+      </select>
+    </div>
   </div>
-</div>
-<br>
-<label>Ciudad y Estado, o C.P.</label>
-<input type="text" name="CEoCP">
-<label>Rango De Busqueda</label><br>
-<select>
-  <option value="0">Sin Rango</option>
-  <option value="1">5km</option>
-  <option value="2">10km</option>
-  <option value="3">20km</option>
-</select>
-<label>Rango de Precio De : - A:</label>
-<div class="row">
-  <div class="De">
-    <select>
-      <option value="0">Todos</option>
-      <option value="1">100,000</option>
-      <option value="2">500,000</option>
-      <option value="3">1,000,000</option>
-      <option value="4">2,000,000</option>
-    </select>
+  <label>Recámara(s)</label><br>
+  <select>
+    <option value="0">No importa.</option>
+    <option value="1">3</option>
+    <option value="2">4</option>
+    <option value="3">5</option>
+  </select>
+  <br><label>Baño(s)</label><br>
+  <select>
+    <option value="0">No importa.</option>
+    <option value="1">1</option>
+    <option value="2">1 y medio</option>
+    <option value="3">2</option>
+    <option value="4">3</option>
+  </select>
+  <div class="row">
+    <div class="buton_busqueda">
+      <input type="submit" value="Buscar">
+    </div>
   </div>
-  <div class="A">
-    <select>
-      <option value="0">Todos</option>
-      <option value="1">100,000</option>
-      <option value="2">500,000</option>
-      <option value="3">1,000,000</option>
-      <option value="4">2,000,000</option>
-    </select>
-  </div>
-</div>
-<label>Recámara(s)</label><br>
-<select>
-  <option value="0">No importa.</option>
-  <option value="1">3</option>
-  <option value="2">4</option>
-  <option value="3">5</option>
-</select>
-<br><label>Baño(s)</label><br>
-<select>
-  <option value="0">No importa.</option>
-  <option value="1">1</option>
-  <option value="2">1 y medio</option>
-  <option value="3">2</option>
-  <option value="4">3</option>
-</select>
-<div class="row">
-  <div class="button">
-    <button>Buscar</button>
-  </div>
-</div>
+</form>
