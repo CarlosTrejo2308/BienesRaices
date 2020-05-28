968d2b2df349c759cd0a4b1a9e1c28e7c4a4be3d
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2020-05-27 20:44:16 -0500

    controler de las busquedas

---
+++ controller/lookup.php
@@ -0,0 +1,12 @@
+<?php
+include_once '../models/dbh.php';
+
+$lugar = $_POST[''];
+$rango = $_POST[''];
+$precioMin = $_POST[''];
+$precioMax = $_POST[''];
+$recamaras = $_POST[''];
+$banos = $_POST[''];
+
+
+ ?>
