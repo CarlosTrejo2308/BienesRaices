46d357413b91890667263865f7095e067c647547
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2020-05-28 22:18:14 -0500

    Modulo de tarjetas

--- views/busqueda.php
+++ views/busqueda.php
@@ -12,8 +12,7 @@
 </head>
 <body>
 	<?php
-
-
+	// Aqui obtenemos la variable de la pagina lookup y la imprimimos
 	$resultados = $_SESSION['resultado'];
 
 	foreach ($resultados as &$valor) {
@@ -33,6 +32,15 @@
 							echo("This is a photo");
 
 						?>
+
+						<!-- Aqui ponemos el embedded de google maps -->
+						<p>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.2020140005457!2d-100.93931268487543!3d25.531646983741652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDMxJzUzLjkiTiAxMDDCsDU2JzEzLjciVw!5e0!3m2!1sen!2smx!4v1590715283640!5m2!1sen!2smx" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+						</p>
+
+						<p>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21223.125789040376!2d-100.95906143314131!3d25.474036254138124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86886d35b3ecf4f7%3A0x961833e02e71504!2sVirreyes%20Residencial%2C%2025230%20Saltillo%2C%20Coahuila!5e0!3m2!1sen!2smx!4v1590715615023!5m2!1sen!2smx" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+						</p>
 					</div>
 				</div>
 
